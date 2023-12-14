9f73694d6d2ba530230e7ff0fc9ab93d88710c13
Yago Marques <yagoapp2013@gmail.com>
2023-12-13 22:17:44 -0300

    add js part 1

---
+++ js/segregador.js
@@ -0,0 +1,11 @@
+console.log('Olá mundo, intrudoução a JS')
+console.log(`data hora: ${new Date()}`)
+
+function mensagem() {
+    alert('Chamamos um modal')
+}
+
+
+function msg2(){
+    alert(`data hora: ${new Date()}`)
+}
