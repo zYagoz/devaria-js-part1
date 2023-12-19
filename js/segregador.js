3bd53c3d60136d522c346fc7fb0f1d0e85dd43c8
Yago Marques <yagoapp2013@gmail.com>
2023-12-18 21:09:39 -0300

    operacoes logicas

--- js/segregador.js
+++ js/segregador.js
@@ -1,11 +1,46 @@
-console.log('Olá mundo, intrudoução a JS')
-console.log(`data hora: ${new Date()}`)
+const olaMundo ='Olá Mundo, introdução a JS';
+const dataAtual = new Date();
 
-function mensagem() {
-    alert('Chamamos um modal')
+console.log('Olá mundo, intrudoução a JS');
+console.log(`data hora: ${dataAtual}`);
+
+function mensagem(mensagem) {
+    alert(mensagem);
 }
 
 
 function msg2(){
-    alert(`data hora: ${new Date()}`)
+    mensagem('aprendenddo funções');
 }
+
+
+const mensagemFinal = (mensagemFinal) => {
+    console.log(mensagemFinal);
+}
+
+const verdadeiro = true;
+const naoverdadeiro = !verdadeiro;
+
+const tudoEverdadeiro = verdadeiro && naoverdadeiro;
+const tudoEfalso = !verdadeiro && naoverdadeiro;
+const algumEverdadeiro = verdadeiro || naoverdadeiro
+const algumEfalso = verdadeiro || naoverdadeiro
+const alltrue = verdadeiro && !naoverdadeiro
+const allfalse = !verdadeiro && naoverdadeiro
+const firstrue = verdadeiro && naoverdadeiro
+const secondtrue = !verdadeiro || !naoverdadeiro
+
+
+
+console.log('verdadeiro' , verdadeiro);
+console.log('naoverdadeiro', naoverdadeiro);
+console.log('tudoEverdadeiro', tudoEverdadeiro);
+console.log('tudoEfalso', tudoEfalso);
+console.log('algumEverdadeiro', algumEverdadeiro);
+console.log('algumEfalso', algumEfalso);
+console.log('alltrue', alltrue);
+console.log('allfalse', allfalse);
+console.log('firstrue', firstrue);
+console.log('secondtrue', secondtrue);
+
+
